3d7cbf94e8e01e2d8b69e1bacb067a3bfb99c7b6
Abrar Saad <abrarsaad3572022@gmail.com>
2024-08-15 02:50:07 +0300

    2. Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,7 @@ import uuid
 from typing import Union, Callable, Optional
 from functools import wraps
 
+
 def count_calls(method: Callable) -> Callable:
     """
     Decorator that counts the number of times a method is called.
@@ -14,9 +15,11 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+
 def call_history(method: Callable) -> Callable:
     """
-    Decorator that stores the history of inputs and outputs for a particular function.
+    Decorator that stores the history of inputs
+      and outputs for a particular function.
     """
     @wraps(method)
     def wrapper(self, *args, **kwargs):
@@ -27,6 +30,7 @@ def call_history(method: Callable) -> Callable:
         self._redis.rpush(output_key, str(output))
         return output
     return wrapper
+
 
 def replay(method: Callable):
     """
@@ -42,7 +46,9 @@ def replay(method: Callable):
 
     print(f"{method_name} was called {len(inputs)} times:")
     for inp, out in zip(inputs, outputs):
-        print(f"{method_name}(*{inp.decode('utf-8')}) -> {out.decode('utf-8')}")
+        print(f"{method_name}(*{inp.decode('utf-8')}) ->
+              {out.decode('utf-8')}")
+
 
 class Cache:
     def __init__(self):
@@ -56,7 +62,8 @@ class Cache:
         self._redis.set(random_key, data)
         return random_key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, None]:
+    def get(self, key: str, fn: Optional[Callable] = None) ->
+    Union[str, bytes, int, None]:
         data = self._redis.get(key)
         if data is None:
             return None
